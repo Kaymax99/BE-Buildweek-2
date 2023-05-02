9ab2943136ac06e984d7fa867b40f0ecdd96618e
Simone Basile <Brink923@hotmail.it>
2023-05-02 10:22:21 +0200

    Initial project setup

---
+++ src/main/java/com/BEBuildweek2/BeBuildweek2Application.java
@@ -0,0 +1,13 @@
+package com.BEBuildweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBuildweek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBuildweek2Application.class, args);
+	}
+
+}
