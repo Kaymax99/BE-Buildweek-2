0353ea5b9c4579261c969eb94244f0c6e04998b8
Simone Basile <Brink923@hotmail.it>
2023-05-02 10:22:21 +0200

    Initial project setup

---
+++ src/test/java/com/BEBuildweek2/BeBuildweek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BEBuildweek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeBuildweek2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
